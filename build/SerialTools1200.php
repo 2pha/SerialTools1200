88d1f7d5ea076aaab59394c120a5cc56e7f7c74f
Chris Brown <chris@2pha.com>
2017-07-16 19:24:53 +1000

    authors.

--- build/SerialTools1200.php
+++ build/SerialTools1200.php
@@ -6,6 +6,14 @@ public function isValid($serial) {
 public function isNewSerialFormat($serial) {
 return substr(strtolower($serial), 0, 2) === 'ge';
 }
+public function authors() {
+$authors = array('Chris "2pha" Brown (2pha.com)');
+$authors->forEach(function ($value) {
+$console->log($value);
+}
+);
+return $authors;
+}
 
 }
 
--- src/SerialTools1200.js
+++ src/SerialTools1200.js
@@ -8,4 +8,12 @@ class SerialTools1200 {
     return serial.toLowerCase().substr(0,2) === 'ge';
   }
 
+  authors() {
+    let authors = ['Chris "2pha" Brown (2pha.com)'];
+    authors.forEach(function(value){
+      console.log(value);
+    });
+    return authors;
+  }
+
 }
