b07b3a706923b0d0eaf12ed16707f2c833efa7b1
Chris Brown <chris@2pha.com>
2017-07-21 16:19:31 +1000

    Added copyright to code, removed index.html test file.

--- build/SerialTools1200.php
+++ build/SerialTools1200.php
@@ -19,6 +19,23 @@ public function authors() {
 $authors = array('Chris "2pha" Brown (2pha.com)');
 return $authors;
 }
+public function copyright() {
+return "
+    Copyright 2017 Chris Brown http://2pha.com
+
+    Licensed under the Apache License, Version 2.0 (the 'License');
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an 'AS IS' BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+    ";
+}
 
 }
 
